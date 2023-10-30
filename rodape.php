9ff3a38b7c925c3a7280188b6d7448f6f527baf9
andrecrimsom <andre.faquineti@etec.sp.gov.br>
2023-10-30 20:41:16 -0300

    pacienci

---
+++ rodape.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <title>VittaClinic</title>
+    </head>
+    <body>
+        <?php
+        echo '<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>';
+        echo 'rodape';
+        ?>
+    </body>
+</html>
