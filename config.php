7451787adfdd91e46c63495632252df3a7c0bcfa
andrecrimsom <andre.faquineti@etec.sp.gov.br>
2023-11-22 20:16:33 -0300

    a

--- config.php
+++ config.php
@@ -10,8 +10,10 @@
     try {
         $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        echo "Conexão com o banco de dados bem-sucedida"
-    } catch(PDOException $e) {
+        echo "Conexão com o banco de dados bem-sucedida";
+    }
+    
+    catch(PDOException $e) {
         echo "Erro na conexão: " . $e->getMessage();
     }
 ?>
