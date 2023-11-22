c69dc6ef8d00fd5709674082e762bcf809d5b7da
andrecrimsom <andre.faquineti@etec.sp.gov.br>
2023-11-22 20:14:03 -0300

    .

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
     session_start();
 
     $servername = "192.168.8.6";
-    $username = "vittaclinic@192.168.8.6";
+    $username = "vittaclinic";
     $password = "amd123";
     $dbname = "vittaclinic";
     
@@ -10,6 +10,7 @@
     try {
         $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        echo "Conexão com o banco de dados bem-sucedida"
     } catch(PDOException $e) {
         echo "Erro na conexão: " . $e->getMessage();
     }
