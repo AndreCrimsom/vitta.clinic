3d217d6f830c4dff73aab06a0c561bc882defcf5
andrecrimsom <andre.faquineti@etec.sp.gov.br>
2023-11-22 20:29:15 -0300

    .

--- novologin.php
+++ novologin.php
@@ -15,5 +15,16 @@
 
 <body>
     <div>
+        <h2>Formulário</h2>
+        <form method="post" action="salvar_dados.php">
+            <label for="nome">Nome:</label>
+            <input type="text" name="nome" id="nome" required><br><br>
+            
+            <label for="email">Email:</label>
+            <input type="email" name="email" id="email" required><br><br>
+            
+            <input type="submit" value="Enviar">
+        </form>
+    </div>
 </body>
 </html>
