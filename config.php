de7982cbda85256d4e7da796ed27d6e57bfdc4f5
André Gabriel <131073462+AndreCrimsom@users.noreply.github.com>
2023-11-27 19:21:32 -0300

    Update config.php

--- config.php
+++ config.php
@@ -8,7 +8,6 @@
     try {
         $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        echo "Conexão com o banco de dados bem-sucedida";
     }
     
     catch(PDOException $e) {
